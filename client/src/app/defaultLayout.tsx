78fa8f9471c73dde6dd9c01127fdc85940f328b9
Arian Shaikh <ariansk90@gmail.com>
2023-07-20 00:01:55 +0530

    added signup and login page

--- client/src/app/defaultLayout.tsx
+++ client/src/app/defaultLayout.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "@/components/navbar/Navbar";
 import Sidebar from "@/components/navbar/Sidebar";
-import { useSession } from "next-auth/react";
 import LoginForm from "@/components/login";
 import LoadingSpinner from "@/components/loadingSpinner/LoadingSpinner";
 
@@ -11,28 +10,39 @@ type Props = {};
 
 const DefaultLayout = ({ children }: any) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const session = useSession();
+  let [user, setUser] = useState({});
 
   const toggleSidebar = () => {
     console.log("toggle sidebar");
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  if (session.status === "unauthenticated") {
-    return <LoginForm />;
-  } else if(session.status === "loading"){
-    return <LoadingSpinner />
-  } else {
-    return (
-      <div className="flex">
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className="flex flex-col justify-start items-center w-full">
-          <Navbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-          <div className="lg:w-[65%] w-[90%]">{children}</div>
+  useEffect(()=>{
+    // @ts-ignore
+    let user = JSON.parse(localStorage.getItem("user"));
+    if(!user){
+      
+    }
+    if(user && user._id){
+      setUser(user);
+    }
+  }, [])
+
+  return (
+    <div>
+      {/* @ts-ignore */}
+      {user && user._id ? (
+        <div className="flex">
+          <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+          <div className="flex flex-col justify-start items-center w-full">
+            <Navbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            <div className="lg:w-[65%] w-[90%]">{children}</div>
+          </div>
         </div>
-      </div>
-    );
-  }
+      ): (<LoginForm />)}
+    </div>
+  );
+  
 };
 
 export default DefaultLayout;
