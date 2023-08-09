9b3b30dcea8e2e75293fe8268d083ad4893abdf7
Tuhin <tuhinbr99@gmail.com>
2023-08-09 22:56:48 +0530

    and some more changes

--- client/src/components/card/EditBooking.tsx
+++ client/src/components/card/EditBooking.tsx
@@ -364,6 +364,7 @@ interface Props {
               id="Advancedate"
               name="Advancedate"
               type="date"
+              max={editingBookingData.checkInDate.split("T")[0]}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="24.05.26"
               required
