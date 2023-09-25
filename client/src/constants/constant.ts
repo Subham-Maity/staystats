5b8d69a19095c275212c96b45d0cff4661dc1a27
Tech-Sayngo <141753444+Tech-Sayngo@users.noreply.github.com>
2023-09-25 22:14:16 +0530

    Update constant.ts

--- client/src/constants/constant.ts
+++ client/src/constants/constant.ts
@@ -5,4 +5,4 @@
 // export const FRONTEND_URL = "http://localhost:3000";
 
 export const BASE_URL = "https://sayngo-backend.onrender.com";
-export const FRONTEND_URL = "https://www.livebookingsayngo247.com/";
+export const FRONTEND_URL = "https://www.livebookingsayngo247.com";
