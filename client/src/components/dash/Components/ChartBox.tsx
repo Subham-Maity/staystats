a4192208dc1b546585f85bd92482aa3c14187381
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-10-11 00:29:21 +0530

    Dashboard Dataset's Top Portion Ready

--- client/src/components/dash/Components/ChartBox.tsx
+++ client/src/components/dash/Components/ChartBox.tsx
@@ -1,62 +1,80 @@
 "use client";
-
 import "./chartBox.scss";
 import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 import Link from "next/link";
+import { FaUserClock } from "react-icons/fa";
 
 type Props = {
   color: string;
-  icon: string;
+  icon?: string;
   title: string;
   dataKey: string;
   number: number | string;
   percentage: number;
   chartData: object[];
+  reactIcon?: string;
+  titleOfPercentage: string;
 };
 
 const ChartBox = (props: Props) => {
+  const getReactIcon = (iconName: string) => {
+    switch (iconName) {
+      case "BsCalendar2Date":
+        return <FaUserClock className="text-lg" />;
+      default:
+        return null;
+    }
+  };
+
   return (
-      <div className="chartBox">
-        <div className="boxInfo">
-          <div className="title">
-            <img src={props.icon} alt="" />
-            <span>{props.title}</span>
-          </div>
-          <h1>{props.number}</h1>
-          <Link href="/" style={{ color: props.color }}>
-            View all
-          </Link>
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          {props.reactIcon && getReactIcon(props.reactIcon)}
+          {props.icon && <img src={props.icon} alt="" />}
+          <span>{props.title}</span>
         </div>
-        <div className="chartInfo">
-          <div className="chart">
-            <ResponsiveContainer width="99%" height="100%">
-              <LineChart data={props.chartData}>
-                <Tooltip
-                    contentStyle={{ background: "transparent", border: "none" }}
-                    labelStyle={{ display: "none" }}
-                    position={{ x: 10, y: 70 }}
-                />
-                <Line
-                    type="monotone"
-                    dataKey={props.dataKey}
-                    stroke={props.color}
-                    strokeWidth={2}
-                    dot={false}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-          <div className="texts">
+        <h1>{props.number}</h1>
+        <Link href="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: -40 }}
+                formatter={(value, name, props) => {
+                  // Display the exact date as a tooltip
+                  return props.payload && props.payload.name
+                    ? new Date(props.payload.name).toLocaleDateString()
+                    : value;
+                }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
           <span
-              className="percentage"
-              style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
           >
             {props.percentage}%
           </span>
-            <span className="duration">this month</span>
-          </div>
+          <span className="duration">{props.titleOfPercentage}</span>
         </div>
       </div>
+    </div>
   );
 };
 
--- client/src/components/dash/Dashboard.tsx
+++ client/src/components/dash/Dashboard.tsx
@@ -1,64 +1,80 @@
 import ChartBox from "@/components/dash/Components/ChartBox";
 import {
-  barChartBoxRevenue,
-  barChartBoxVisit,
-  chartBoxConversion,
-  chartBoxProduct,
-  chartBoxRevenue,
-  chartBoxUser,
+  Checkin,
+  Checkout,
+  TodaysBooking,
+  TodaysModifiedBooking,
+  TodaysCancelledBooking,
+  TotalUsers,
+  TotalRevenue,
+  TotalDue,
+  TotalHotels,
 } from "./data";
 import TailwindWrapper from "@/components/dash/Components/TailwindWrapper";
 
 const Dashboard = () => {
-  return <>
-    <div>
-    <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 ">
-      <TailwindWrapper className={'mt-5'}>
-        <ChartBox {...chartBoxUser} />
-      </TailwindWrapper>
+  return (
+    <>
+      <div>
+        <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-3 p-8">
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <ChartBox titleOfPercentage="This Week" {...Checkin} />
+          </TailwindWrapper>
 
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...Checkout} />
+            </div>
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...TodaysBooking} />
+            </div>
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox
+                titleOfPercentage="This Week"
+                {...TodaysModifiedBooking}
+              />
+            </div>
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <ChartBox
+              titleOfPercentage="This Week"
+              {...TodaysCancelledBooking}
+            />
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...TotalUsers} />
+            </div>
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...TotalRevenue} />
+            </div>
+          </TailwindWrapper>
+
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...TotalDue} />
+            </div>
+          </TailwindWrapper>
+          <TailwindWrapper className={"mt-5 justify-self-center"}>
+            <div className="box box2">
+              <ChartBox titleOfPercentage="This Week" {...TotalHotels} />
+            </div>
+          </TailwindWrapper>
         </div>
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
-        </div>
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
-        </div>
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <ChartBox {...chartBoxUser} />
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
-        </div>
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
-        </div>
-      </TailwindWrapper>
-
-      <TailwindWrapper className={'mt-5'}>
-        <div className="box box2">
-          <ChartBox {...chartBoxUser} />
-        </div>
-      </TailwindWrapper>
-    </div>
-    </div>
-  </>;
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
