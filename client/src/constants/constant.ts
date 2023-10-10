f989fa4a39bb9bae512115536038ae35d3925a34
Tuhin <tuhinbr99@gmail.com>
2023-10-10 15:29:29 +0530

    Added self and admin work generate

--- client/src/constants/constant.ts
+++ client/src/constants/constant.ts
@@ -1,8 +1,8 @@
 // utils/constants.ts
 
 // comment out the one you don't want to use
-export const BASE_URL = "http://localhost:5000";
-export const FRONTEND_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:5000";
+// export const FRONTEND_URL = "http://localhost:3000";
 
-// export const BASE_URL = "http://api.webcrack.online:5000";
-// export const FRONTEND_URL = "https://staystats.vercel.app";
+export const BASE_URL = "https://stay-stats-backend.onrender.com";
+export const FRONTEND_URL = "https://staystats.vercel.app";
