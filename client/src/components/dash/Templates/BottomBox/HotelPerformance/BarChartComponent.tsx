723145fd0b85b113d593b24ba0600055ea0ac483
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-11-19 04:58:34 +0530

    Responsiveness,Text Fix , Color Gradient , BarSize , Rounded ,TooltipStyle

--- client/src/components/dash/Templates/BottomBox/HotelPerformance/BarChartComponent.tsx
+++ client/src/components/dash/Templates/BottomBox/HotelPerformance/BarChartComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import {
     ComposedChart,
     Bar,
@@ -7,51 +7,51 @@ import {
     Tooltip,
     Legend,
     ResponsiveContainer,
+
 } from 'recharts';
-import Context from "@/context/Context";
+import Context from '@/context/Context';
 
-
-
-function BarChartComponent(props:any) {
+function BarChartComponent(props: any) {
     const { isDarkTheme } = useContext(Context);
     const customTooltipStyle = {
         backgroundColor: isDarkTheme ? '#000' : '#fff',
-        border: 'none',
+        border: isDarkTheme ? '1px solid #fff' : '1px solid #000',
         color: isDarkTheme ? '#fff' : '#000',
-        fontSize: '10px',
+        fontSize: '15px',
+        borderRadius: '10px',
     };
 
     const yAxisTextStyle = {
         fill: isDarkTheme ? '#fff' : '#000',
-        fontSize: '10px', // Adjust font size as needed
+        fontSize: '10px',
     };
     const xAxisTextStyle = {
         fill: isDarkTheme ? '#fff' : '#000',
-        fontSize: '15px', // Adjust font size as needed
+        fontSize: '15px',
     };
 
     return (
-        <div style={{ height: '350px', overflow: 'auto'}}>
+        <div style={{ height: '350px', overflow: 'auto' }}>
             <ResponsiveContainer width="100%" height={400}>
                 <ComposedChart data={props.chartData} layout="vertical">
-                    {/* Remove CartesianGrid to remove the grid */}
-                    <YAxis dataKey="source" type="category" tick={yAxisTextStyle} />
+                    <YAxis interval={0} dataKey="source" textAnchor="end" type="category" tick={yAxisTextStyle} />
                     <XAxis type="number" tick={xAxisTextStyle} />
                     <Tooltip contentStyle={customTooltipStyle} />
                     <Legend />
+                    <defs>
+                        <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.1} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={1} />
+                        </linearGradient>
+                    </defs>
                     <Bar
                         dataKey={props.type}
                         fill="url(#colorGradient)"
                         stroke="#006ef5"
-                        strokeWidth={1}
+                        strokeWidth={0.5}
                         barSize={20}
+                        radius={[0, 5, 5, 0]}
                     />
-                    <defs>
-                        <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
-                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0.1} />
-                        </linearGradient>
-                    </defs>
                 </ComposedChart>
             </ResponsiveContainer>
         </div>
--- client/src/components/dash/Templates/BottomBox/LocationPerformance/BarChartComponent.tsx
+++ client/src/components/dash/Templates/BottomBox/LocationPerformance/BarChartComponent.tsx
@@ -1,20 +1,101 @@
-import React from 'react';
-import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
+import Context from '@/context/Context';
 
 function BarChartComponent(props: any) {
+    const { isDarkTheme } = useContext(Context);
+    const [xAxisAngle, setXAxisAngle] = useState(0);
+    const [chartWidth, setChartWidth] = useState(600); // Initial width for larger screens
+
+    const customTooltipStyle = {
+        backgroundColor: isDarkTheme ? '#000' : '#fff',
+        border: isDarkTheme ? '1px solid #fff' : '1px solid #000',
+        color: isDarkTheme ? '#fff' : '#000',
+        fontSize: '15px',
+        borderRadius: '10px',
+    };
+
+    const yAxisTextStyle = {
+        fill: isDarkTheme ? '#fff' : '#000',
+        fontSize: '10px',
+    };
+
+    const xAxisTextStyle = {
+        fill: isDarkTheme ? '#fff' : '#000',
+        fontSize: '15px',
+        textAnchor: 'middle',
+
+    };
+
+    const updateChartSettings = () => {
+        const screenWidth = window.innerWidth;
+        console.log(screenWidth + 'px');
+        if (screenWidth >= 1800) {
+            setXAxisAngle(0); // Adjust as needed
+
+            setChartWidth(600); // Adjust as needed
+        } else if (screenWidth >= 1024 && screenWidth < 1800) {
+            setXAxisAngle(-25); // Adjust as needed
+            setChartWidth(600); // Adjust as needed
+
+
+
+        } else if (screenWidth < 768) {
+            setXAxisAngle(-50); // Adjust as needed
+            setChartWidth(screenWidth - 20); // Adjust as needed
+        } else if (screenWidth >= 768 && screenWidth < 1024) {
+            setXAxisAngle(-25); // Adjust as needed
+            setChartWidth(screenWidth - 100); // Adjust as needed
+        } else {
+            setXAxisAngle(-25); // Default angle for larger screens
+            setChartWidth(600); // Default width for larger screens
+        }
+    };
+
+    useEffect(() => {
+        updateChartSettings();
+        window.addEventListener('resize', updateChartSettings);
+        return () => window.removeEventListener('resize', updateChartSettings);
+    }, []);
+
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={props.chartData}>
+        <ResponsiveContainer width="100%" height={400}>
+            <BarChart data={props.chartData} width={chartWidth}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="source" />
-                <YAxis />
-                <Tooltip />
+                <XAxis
+                    dataKey="source"
+                    angle={xAxisAngle}
+                    textAnchor="end"
+                    tick={xAxisTextStyle}
+                    interval={0}
+                />
+                <YAxis tick={yAxisTextStyle} />
+                <Tooltip
+                    contentStyle={customTooltipStyle}
+                    isAnimationActive={true}
+                    useTranslate3d={true}
+                    animationEasing={'ease-in-out'}
+                />
                 <Legend />
-                <Bar dataKey={props.type} fill="#8884d8" stroke="#006ef5" strokeWidth={3} />
+                <Bar
+                    dataKey={props.type}
+                    fill="url(#colorGradient)"
+                    stroke="#006ef5"
+                    strokeWidth={0.5}
+                    barSize={40}
+                />
             </BarChart>
         </ResponsiveContainer>
     );
 }
 
 export default BarChartComponent;
-
