b0142987a316fda380fe8eb3fe09ed36b86a1da1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-03-14 03:02:11 +0530

    feat: stay = checkin date

--- client/src/context/Context.tsx
+++ client/src/context/Context.tsx
@@ -1,38 +1,42 @@
 import React from "react";
 
 export interface InfoData {
-    softCap: number;
+  softCap: number;
 }
 
 export interface ContextState {
-    infoData: InfoData;
-    isDarkTheme: Boolean;
-    isGrid: Boolean;
+  infoData: InfoData;
+  isDarkTheme: Boolean;
+  date: any;
+  isGrid: Boolean;
 }
 
 export interface ContextDispatch {
-    setInfoData: React.Dispatch<React.SetStateAction<InfoData>>;
-    setIsDarkTheme: React.Dispatch<React.SetStateAction<Boolean>>;
-    setIsGrid: React.Dispatch<React.SetStateAction<Boolean>>;
+  setInfoData: React.Dispatch<React.SetStateAction<InfoData>>;
+  setIsDarkTheme: React.Dispatch<React.SetStateAction<Boolean>>;
+  setDate: React.Dispatch<React.SetStateAction<any>>;
+  setIsGrid: React.Dispatch<React.SetStateAction<Boolean>>;
 }
 
 type ContextProps = ContextState & ContextDispatch;
 
 const defaultInfoData: InfoData = {
-    softCap: 0,
+  softCap: 0,
 };
 
 const defaultDispatch: ContextDispatch = {
-    setInfoData: () => {},
-    setIsDarkTheme: () => {},
-    setIsGrid: () => {},
+  setInfoData: () => {},
+  setIsDarkTheme: () => {},
+  setDate: () => {},
+  setIsGrid: () => {},
 };
 
 const defaultFormContext: ContextProps = {
-    ...defaultDispatch,
-    infoData: defaultInfoData,
-    isDarkTheme: false,
-    isGrid: true,
+  ...defaultDispatch,
+  infoData: defaultInfoData,
+  isDarkTheme: false,
+  date: new Date(),
+  isGrid: true,
 };
 
 const Context = React.createContext<ContextProps>(defaultFormContext);
