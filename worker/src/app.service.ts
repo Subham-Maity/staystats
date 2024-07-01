d72aef9ef8055f8bf1b52442c849db7d74b354c1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-07-02 01:20:39 +0530

    feat: server url change

--- worker/src/app.service.ts
+++ worker/src/app.service.ts
@@ -1,19 +1,26 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
+  private readonly mainServer: string;
+  private readonly workerServer: string;
+  constructor(private configService: ConfigService) {
+    this.mainServer = this.configService.get<string>('MAIN_SERVER');
+    this.workerServer = this.configService.get<string>('WORKER_SERVER');
+  }
 
   getHello(): string {
     return 'Hello World!';
   }
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_5_MINUTES)
   async keepServer1Warm() {
     try {
-      const serverUrl = `http://localhost:5000`;
+      const serverUrl = this.mainServer;
       await axios.get(serverUrl);
       this.logger.log(`Sent request to ${serverUrl} to keep the server warm`);
     } catch (error) {
@@ -23,10 +30,10 @@ export class AppService {
       );
     }
   }
-  @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_10_SECONDS)
   async keepServer2Warm() {
     try {
-      const serverUrl = `http://localhost:3333`;
+      const serverUrl = this.workerServer;
       await axios.get(serverUrl);
       this.logger.log(`Sent request to ${serverUrl} to keep the server warm`);
     } catch (error) {
