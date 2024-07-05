28749871a14601ff6e6e0c83454883ecf248236b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-07-05 16:32:25 +0530

    fix: frontend url

--- client/src/constants/constant.ts
+++ client/src/constants/constant.ts
@@ -2,10 +2,10 @@
 
 // comment out the one you don't want to use
 // export const BASE_URL = "http://localhost:5000";
-export const FRONTEND_URL = "http://localhost:3000";
+// export const FRONTEND_URL = "http://localhost:3000";
 
 export const BASE_URL = "https://sayngo-prod-server-obd3.onrender.com";
-// export const FRONTEND_URL = "https://staystats.vercel.app";
+export const FRONTEND_URL = "https://staystats.vercel.app";
 
 // export const BASE_URL = "https://www.webcrack.online";
 // export const FRONTEND_URL = "https://www.livebookingsayngo247.com";
